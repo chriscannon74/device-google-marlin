bd31f989964fa03496e7363a20757c110323e315
Dan Pasanen <dan.pasanen@gmail.com>
2017-01-05 20:20:40 -0600

    marlin: add hardware key overlays
    create overlay-aosip but dont use at this time
    
    Change-Id: I3c275ba340000702dc60a45a683d22465463c490

---
+++ device-aosip.mk
@@ -0,0 +1,12 @@
+<<<<<<< HEAD
+=======
+# Fingerprint sensor type
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.fingerprint=fpc
+
+# Overlays
+#DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-aosip
+
+# Update engine
+PRODUCT_PACKAGES += brillo_update_payload
+>>>>>>> cb64866... marlin: add hardware key overlays
