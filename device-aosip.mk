ec276bf92f2fd5b2f75a847f6565b4d3cde9d2be
Dan Pasanen <dan.pasanen@gmail.com>
2017-03-01 09:10:41 -0600

    marlin: add nexus.xml for nexus/pixel experience
    Change-Id: I040d65bbe82f8a255f30dcebe760d4ced48e71b4

--- device-aosip.mk
+++ device-aosip.mk
@@ -22,6 +22,21 @@ PRODUCT_PACKAGES += \
 # Overlays
 #DEVICE_PACKAGE_OVERLAYS += device/google/marlin/overlay-aosip
 
+<<<<<<< HEAD
+=======
+# Pixel Experience
+PRODUCT_COPY_FILES += \
+  device/google/marlin/nexus.xml:system/etc/sysconfig/nexus.xml
+
+# Tethering
+PRODUCT_PROPERTY_OVERRIDES += \
+    net.tethering.noprovisioning=true
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
+
+>>>>>>> 59174c5... marlin: add nexus.xml for nexus/pixel experience
 # Update engine
 PRODUCT_PACKAGES += brillo_update_payload
 >>>>>>> cb64866... marlin: add hardware key overlays
